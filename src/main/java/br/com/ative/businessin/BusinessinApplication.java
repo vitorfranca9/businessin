f050e559d82e0ede57beadc9e55e362116e427e0
vfranca9 <vitorfranca9@gmail.com>
2016-08-27 20:39:22 -0300

    Project Build

---
+++ src/main/java/br/com/ative/businessin/BusinessinApplication.java
@@ -0,0 +1,12 @@
+package br.com.ative.businessin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessinApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/ative/businessin/BusinessinApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.ative.businessin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BusinessinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
